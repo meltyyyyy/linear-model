1b330ca3f025f0881c23769704644e2527d88f1a
takeru.abe <takeru.abe@gaiax.com>
2022-01-12 12:26:00 +0900

    lasso model alpha 0.01

--- lasso/lasso.py
+++ lasso/lasso.py
@@ -12,3 +12,10 @@ def execute():
     print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
     print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
     print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+    lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+    print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+    print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+    print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+
+
